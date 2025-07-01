0b9a42c79aa47f3100b22c1c57b7af89e3e34dfc
Himanshu <prjapathimanshu51@gmail.com>
2025-07-01 22:02:17 +0530

    fixed privacy policy issue

--- src/main.jsx
+++ src/main.jsx
@@ -11,11 +11,11 @@ import Service from "./pages/NavPages/Service.jsx";
 import CarBooking from "./pages/NavPages/CarBooking.jsx";
 import Gallery from "./pages/NavPages/Gallery.jsx";
 import Contact from "./pages/NavPages/Contact.jsx";
-// import PrivacyPolicy from "./pages/FooterPages/PrivacyPolicy.jsx";
 import TermsService from "./pages/FooterPages/TermsService.jsx";
 import DetailsPage from "./pages/NavPages/DetailsPage.jsx";
 import Testimonials from "./pages/NavPages/Testimonials.jsx";
 import { HelmetProvider } from "react-helmet-async";
+import PrivacyPolicy from "./pages/FooterPages/PrivacyPolicys.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -27,7 +27,7 @@ const router = createBrowserRouter(
       <Route path="gallery" element={<Gallery />} />
       <Route path="contact" element={<Contact />} />
       <Route path="detail/:id" element={<DetailsPage />} />
-      {/*<Route path="privacy-policy" element={<PrivacyPolicy />} />*/}
+      <Route path="privacy-policy" element={<PrivacyPolicy />} />
       <Route path="terms-of-service" element={<TermsService />} />
       <Route path="testimonials" element={<Testimonials />} />
     </Route>
